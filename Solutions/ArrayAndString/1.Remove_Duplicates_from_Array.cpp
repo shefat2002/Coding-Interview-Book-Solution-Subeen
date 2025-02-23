a0448a33112daaf0a94d6f4e9931278e8ba29da7
shefat2002 <shefat2002@gmail.com>
2025-02-23 15:03:24 +0600

    Add Problem 1

---
+++ Solutions/ArrayAndString/1.Remove_Duplicates_from_Array.cpp
@@ -0,0 +1,32 @@
+/*
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int remove_duplicates(int (&a)[5])
+{
+    int n = sizeof(a)/sizeof(a[0]);
+    int sz = 1;
+    for(int i = 1; i< n ; i++){
+        if(a[i-1]!= a[i]){
+            a[sz] = a[i];
+            sz++;
+        }
+    }
+    return sz;
+}
+
+int main()
+{
+    int a[] = {1,1,2,3,3};
+    // int n = sizeof(a)/sizeof(a[0]);
+    int n = remove_duplicates(a);
+
+    for(int i = 0 ; i < n ; i++){
+        cout << a[i] << ' ';
+    }
+
+}
